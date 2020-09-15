945f14bbe612659ed5968d0fb6a6cd510fe30b88
Mersad Hj <mersadhassanjani@gmail.com>
2020-09-15 18:38:31 +0430

    second commit

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     if (city && country) {
       const api_call = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
       );
       const response = await api_call.json();
       console.log(response);
