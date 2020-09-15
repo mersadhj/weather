11938a6858f6e70f30aacb4eabc48d5935920b3b
Mersad Hj <mersadhassanjani@gmail.com>
2020-09-15 18:30:01 +0430

    Updates

---
+++ precache-manifest.ba0e5f75084fa65b31e59599f3157f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788c00caeef50c7878bb39c200015aa6",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7927195094acbbf93737",
+    "url": "/weather/static/css/main.07835e3b.chunk.css"
+  },
+  {
+    "revision": "dc3933ce9e2413772501",
+    "url": "/weather/static/js/2.5840626c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.5840626c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7927195094acbbf93737",
+    "url": "/weather/static/js/main.c8ad0b2d.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
